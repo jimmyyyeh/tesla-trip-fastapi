b9c159b3ea50bd4d71a43e39f1f33fcda125ab71
Jimmy Yeh <chienfeng0719@gmail.com>
2022-06-24 11:05:51 +0800

    Remove get car model api in car routes

--- src/routes/car.py
+++ src/routes/car.py
@@ -19,6 +19,7 @@ from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
 
 from core.car_handler import CarHandler
+from core.car_model_handler import CarModelHandler
 from database.db_handler import DBHandler
 from utils import response_models
 from utils.auth_tools import AuthValidator
@@ -27,14 +28,6 @@ from utils.response_models import ResponseHandler
 
 router = APIRouter(prefix='/cars', tags=['car'])
 general_auth = AuthValidator()
-
-
-@router.get('/car-models', response_model=response_models.CarModel)
-async def get_car_model(user: dict = Depends(general_auth), db: Session = Depends(DBHandler.get_db)):
-    result = await CarHandler.get_car_models(
-        db=db
-    )
-    return ResponseHandler.response(result=result)
 
 
 @router.get('/{car_id}', response_model=response_models.Car)
